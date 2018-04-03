4a938fa6714b0857266ca5ee8bf981f80a780efb
RitaJ <ritaj777@gmail.com>
2018-04-03 11:19:02 +0300

    db connection

--- config/database.php
+++ config/database.php
@@ -2,10 +2,10 @@
 
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-$host = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$database = substr($url["path"], 1);
+$host = $url['127.0.0.1'];
+$username = $url['Laravel'];
+$password = $url['1234554321'];
+$database = substr($url['transportas'], 1);
 
 return [
 
